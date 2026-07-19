4b84a3aed6bebc87cf6cc79dda89f4cb808c22e8
preetsagar <ps792001@gmail.com>
2026-07-19 14:57:11 +0530

    update

--- cohort/13. Dynamic Programming/2. Learning Forms and their Basics/Form 4 - Interval DP or LR DP/Merge Elements 1.cpp
+++ cohort/13. Dynamic Programming/2. Learning Forms and their Basics/Form 4 - Interval DP or LR DP/Merge Elements 1.cpp
@@ -2,33 +2,40 @@
 using namespace std;
 
 int n;
-int arr[510];
-int prefix[510];
+int arr[510], prefixSum[510];
+int mod = 100;
 
 int dp[510][510];
 
-int rec(int l, int r){
+int cost(int l, int m, int r){
+    int ll = (prefixSum[m]-prefixSum[l-1])%mod;
+    int rr = (prefixSum[r]-prefixSum[m])%mod;
+
+    return (ll * rr);
+}
+
+int solve(int l, int r){
+    if(l>r) return 0;
     if(l==r) return 0;
 
     if(dp[l][r]!=-1) return dp[l][r];
 
     int ans = 1e9;
-    for(int i=l; i<=r-1; i++){
-        int x = (prefix[i]-prefix[l-1])%100;
-        int y = (prefix[r]-prefix[i])%100;
-        ans = min(ans, rec(l, i) + rec(i+1, r) + x * y);
+    for(int i=l; i<r; i++){
+        ans = min(ans, solve(l, i) + solve(i+1, r) + cost(l,i,r));
     }
+
     return dp[l][r] = ans;
 }
 
-int main() {
+signed main() {
     cin>>n;
-    memset(prefix, 0, sizeof(prefix));
-    memset(dp, -1, sizeof(dp));
     for(int i=1; i<=n; i++){
         cin>>arr[i];
-        prefix[i] = prefix[i-1] + arr[i];
+        prefixSum[i] = prefixSum[i-1]+arr[i];
     }
-    cout<<rec(1, n);
+    memset(dp, -1, sizeof(dp));
+    cout<<solve(1, n);
+
     return 0;
 }
