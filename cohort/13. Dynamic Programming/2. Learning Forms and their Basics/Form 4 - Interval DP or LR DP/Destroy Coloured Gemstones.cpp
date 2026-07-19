b9cf97c6a7b8439d3a9bbe78f3eeba1168e108d4
preetsagar <ps792001@gmail.com>
2026-07-19 21:34:51 +0530

    update

--- cohort/13. Dynamic Programming/2. Learning Forms and their Basics/Form 4 - Interval DP or LR DP/Destroy Coloured Gemstones.cpp
+++ cohort/13. Dynamic Programming/2. Learning Forms and their Basics/Form 4 - Interval DP or LR DP/Destroy Coloured Gemstones.cpp
@@ -1,22 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
+int t,n;
 int arr[110];
-int dp[110][110];
+int dp[100][110];
 
-int rec(int l, int r){
-    if(l>=r) return 1;
+int solve(int l, int r){
+    if(l>r) return 0;
+    if(l==r) return 1;
 
     if(dp[l][r]!=-1) return dp[l][r];
 
     int ans = 1e9;
-    if(arr[l]==arr[r]){
-        ans = min(ans, rec(l+1, r-1));
+    ans = min(ans, 1+solve(l+1, r));
+    if(arr[l]==arr[l+1]){
+        ans = min(ans, 1 + solve(l+2, r));
     }
-    for(int mid=l; mid<r; mid++){
-        if(arr[l]==arr[mid]){
-            ans = min(ans, rec(l+1, mid-1) + rec(mid+1, r));
+    for(int i=l+2; i<=r; i++){
+        if(arr[i]==arr[l]){
+            ans = min(ans, solve(l+1, i-1) + solve(i+1, r));
         }
     }
 
@@ -24,13 +26,13 @@ int rec(int l, int r){
 }
 
 int main() {
-    int t; cin>>t; while(t--){
+    cin>>t; while(t--){
         cin>>n;
+        memset(dp, -1, sizeof(dp));
         for(int i=0; i<n; i++){
             cin>>arr[i];
         }
-        memset(dp, -1, sizeof(dp));
-        cout<<rec(0, n-1)<<'\n';
+        cout<<solve(0, n-1)<<"\n";
     }
     return 0;
 }
